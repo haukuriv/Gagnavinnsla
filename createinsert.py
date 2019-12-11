9efa5f106ee02eb964018f2ce877da6050704ef8
halldor97 <37585235+halldor97@users.noreply.github.com>
2019-12-11 19:06:52 +0000

    Add files via upload

--- createinsert.py
+++ createinsert.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import csv
 import numpy as np
 import time
@@ -32,41 +33,12 @@ def import_file(fileread, delimiter):
 
 
 start = time.time()
-"""
-# Lesa inn gögnin
-#fileread = 'super_hero_powers.csv'
-#fileread = 'heroes_information.csv'
-fileread = 'SuperheroDataset.csv'
-filewrite = 'battle.txt'
-f = open(fileread)
 
-dreader = csv.DictReader(f, delimiter=',')
-
-data = []
-for d in dreader:
-    data.append(d)
-
-f.close()
-
-# Finna unique gildi fyrir alla lykla
-var_set = {}
-
-for keys in data[0].keys():
-    var_set[keys] = set()
-
-for d in data:
-    for keys in d.keys():
-        var_set[keys].add(d[keys])
-
-
-for thekey in var_set.keys():
-    var_set[thekey] = sorted(list(var_set[thekey]))
-"""
 fileread = 'SuperheroDataset.csv'
 filewrite = 'insertstatements.txt'
 
 data, var_set = import_file(fileread, ',')
-data1, var_set1 = import_file('super_hero_powers.csv', ',') 
+data2, var_set2 = import_file('super_hero_powers.csv', ',') 
 
 
 del_me = set() # Þær ofurhetjur sem hafa ekki stats
@@ -74,31 +46,16 @@ del_me = set() # Þær ofurhetjur sem hafa ekki stats
 #del_me.update(set(var_set['Name']).symmetric_difference(var_set1['hero_names']))
 
 stat_str = sorted(['Intelligence','Strength','Speed','Durability','Power','Combat'])
-join_super = sorted(list(set(var_set['Name']).intersection(var_set1['hero_names'])))
+join_super = sorted(list(set(var_set['Name']).intersection(var_set2['hero_names'])))
 # Upphafsstilla
 hero = {}
 
 for thehero in join_super:#var_set['Name']:
     hero[thehero] = {}
+    hero[thehero]['powers'] = []
     for thestat in stat_str:
         hero[thehero][thestat] = ''
-"""
-for d in data:
-    if d['Name'] in join_super:
-        for thekey in d.keys():
-            if thekey in stat_str:
-                print(d['Name'])
-                if d[thekey] == '':
-                    print(d[thekey])
-                    print('Deleting %s' % d['Name'])
-                    print(hero[d['Name']])
-                    print(thekey)
-                    del hero[d['Name']]
-                    break
-                else:
-                    hero[d['Name']][thekey] = d[thekey]
-"""            
-
+         
 del_me = set()
                
 for d in data:
@@ -111,17 +68,28 @@ for d in data:
 
 for thehero in del_me:
     del hero[thehero]
-#    del var_set['Name'][var_set['Name'].index(thehero)]
+
+
+
+Upowers = list(data2[0].keys())
+Upowers = sorted(Upowers[1:])
+
+for d in data2:
+    if d['hero_names'] in hero.keys():
+        for thekey in d.keys():
+            if d[thekey] == 'True' and thekey != 'hero_names':
+                hero[d['hero_names']]['powers'].append(thekey)
+
 
 strats = np.zeros( (6, len(hero)) )
-#for thehero in var_set['Name']:
 
 j = 0
 for thehero in hero:
     i = 0
     for thestrat in hero[thehero]:
-        strats[i, j] = hero[thehero][thestrat]
-        i += 1
+        if thestrat != 'powers':
+            strats[i, j] = hero[thehero][thestrat]
+            i += 1
     j += 1
 
 nr_fights = 10000
@@ -142,8 +110,24 @@ for thehero1 in hero:
     for thehero2 in hero:
         if thehero1 < thehero2:
             #f.write("%s vs %s: %.2f%% chance of winning\n" % (thehero1, thehero2, np.sum(hero[thehero1]['stat/day'] > hero[thehero2]['stat/day']) / nr_fights * 100))
-            f.write("INSERT INTO fights (hero1, hero2, nrwon1, nrwon2, tie) VALUES ('%s', '%s', %i, %i, %i);\n" % (thehero1, thehero2, np.sum(hero[thehero1]['stat/day'] > hero[thehero2]['stat/day']), np.sum(hero[thehero1]['stat/day'] < hero[thehero2]['stat/day']), np.sum(hero[thehero1]['stat/day'] == hero[thehero2]['stat/day'])))
+            f.write("INSERT INTO fights (hero1, hero2, nrwon1, nrwon2, tie) VALUES (%i, %i, %i, %i, %i);\n" % (join_super.index(thehero1)+1, join_super.index(thehero2)+1, np.sum(hero[thehero1]['stat/day'] > hero[thehero2]['stat/day']), np.sum(hero[thehero1]['stat/day'] < hero[thehero2]['stat/day']), np.sum(hero[thehero1]['stat/day'] == hero[thehero2]['stat/day'])))
+
+i = 1
+for thepower in Upowers:
+    f.write("INSERT INTO powers (id, power) VALUES (%i, '%s');\n" % (i, thepower))
+    i += 1
+
+i = 1
+for thehero in hero:
+    for thepower in hero[thehero]['powers']:
+        f.write("INSERT INTO link_power (id, hero_id, power_id) VALUES (%i, %i, %i);\n" % (i, join_super.index(thehero)+1, Upowers.index(thepower)+1))
+        i += 1
+
 f.close()
+
+
+
+
 
 print('\nIt took %.2f seconds to run the code.' % (time.time()-start))
 
